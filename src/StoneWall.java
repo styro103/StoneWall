49cbc81469de0c7563e90e109a2cf12b9514194e
styro103 <smbateng103@yahoo.com>
2014-12-01 09:12:39 -0500

    Main Code Added
    Main code included. Not enough code for working condition of program
    though.

---
+++ src/StoneWall.java
@@ -0,0 +1,38 @@
+/*
+ * Shaun Mbateng
+ * Stone Wall
+ * This program finds the minimum number of blocks needed to build a stone wall given
+ * 	a length and multiple height.
+ * It has a time complexity of O(N).
+ */
+
+import java.util.Scanner; //For User Inputs
+
+public class StoneWall 
+{
+	public static void main(String[] args) 
+	{
+		Scanner cin = new Scanner(System.in); //For Inputs
+		Building Rectangles = new Building(); //Class That Sorts Array
+		int [] heights; //Array to Sort
+		int len; //Length of Array
+		
+		//Enter and Set Array Length
+		System.out.print("Enter the Stone Wall Length: ");
+		len = cin.nextInt();
+		heights = new int [len];
+		
+		//Fill Array
+		for (int i=0; i<len; i++)
+		{
+			System.out.print("Enter Height "+(i+1)+": ");
+			heights[i] = cin.nextInt();
+		}
+		
+		cin.close(); //No More Inputs Needed
+		
+		//Print Arrays
+		System.out.println();
+		System.out.println("Minimum Number of Blocks Needed: "+Rectangles.getBlocks(heights));
+	}
+}
